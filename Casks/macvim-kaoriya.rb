48932c92976ca6af82a5d5c1fab917eee91ce15b
Shintaro Abe <dictav@gmail.com>
2015-12-20 14:18:02 +0900

    Update to 7.4.972-20151216

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'macvim-kaoriya' do
-  version '7.4.712-20150429'
-  sha256 'dc88078d53814eb86c860d7c9bf05d2fb4d7cd4ef76601bdf8518e8643dd4474'
+  version '7.4.972-20151216'
+  sha256 '7fd3650b095bf3068bc7c0564de12f268f71125fe1066af178561072cc907cb6'
 
-  url 'https://github.com/splhack/macvim-kaoriya/releases/download/20150429/macvim-kaoriya-20150429.dmg'
+  url 'https://github.com/splhack/macvim-kaoriya/releases/download/20151216/MacVim-KaoriYa-20151216.dmg'
   homepage 'https://github.com/splhack/macvim-kaoriya'
   license :oss
 
